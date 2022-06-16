1c245b3cdf5c0f6ea75c84888c35a1140fc976a2
Mateus Oliveira da Silva <mateus.osilva@neobpo.com.br>
2022-06-16 14:31:21 -0300

    - atualizacao gitignore - atualiazao vit transformer - Config decoder yaml para modificacao rapida. - decoder exploring - Implementacao do decoder transformer.

--- VIT_backbone/vit_transformer.py
+++ VIT_backbone/vit_transformer.py
@@ -3,70 +3,64 @@ import torch.nn.functional as F
 import yaml
 from torch import nn
 from torch import Tensor
-from torchvision import transforms
-from torchvision.transforms import Compose, Resize, ToTensor
 from einops import rearrange, reduce, repeat
 from einops.layers.torch import Rearrange, Reduce
 
 with open('config_vit.yaml', 'r') as file:
     parameters = yaml.safe_load(file)
 
+
 class PatchEmbedding(nn.Module):
-    
+
     def __init__(self, in_channels: int = parameters['in_channels'],
                  patch_size: int = parameters['patch_size'],
                  emb_size: int = parameters['emb_size'],
                  img_size: int = parameters['img_size']):
-        
         self.patch_size = patch_size
         super().__init__()
         self.projection = nn.Sequential(
-            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),#geração dos patches
+            nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size),  # geração dos patches
             Rearrange('b c (h) (w) -> b (h w) c'),
         )
-        
-        
-        self.cls_token = nn.Parameter(torch.randn(1,1, emb_size))
-        
-        
-        #flat patches
-        self.positions = nn.Parameter(torch.randn((img_size // patch_size) **2 + 1, emb_size))
-        
-        
+
+        self.cls_token = nn.Parameter(torch.randn(1, 1, emb_size))
+
+        # flat patches
+        self.positions = nn.Parameter(torch.randn((img_size // patch_size) ** 2 + 1, emb_size))
+
     def forward(self, x: Tensor):
         """
         debug: Parece que o modelo ta associando o mesmo cls_token para cada patch da imagem.
         """
         b, _, _, _ = x.shape
-        
+
         x = self.projection(x)
-        
+
         cls_tokens = repeat(self.cls_token, '() n c -> b n c', b=b)
         # prepend the cls token to the input
-        
+
         x = torch.cat([cls_tokens, x], dim=1)
         # add position embedding
         x += self.positions
         return x
-    
+
+
 class MultiHeadAttention(nn.Module):
     def __init__(self, emb_size: int = parameters['emb_size_multi_head_attention'],
                  num_heads: int = parameters['num_heads'],
                  dropout: float = parameters['dropout']):
-        
         super().__init__()
-        
+
         self.emb_size = emb_size
         self.num_heads = num_heads
         self.keys = nn.Linear(emb_size, emb_size)
         self.queries = nn.Linear(emb_size, emb_size)
         self.values = nn.Linear(emb_size, emb_size)
-        
+
         self.att_drop = nn.Dropout(dropout)
         self.projection = nn.Linear(emb_size, emb_size)
-        
-        
-    def forward(self, x : Tensor, mask: Tensor = None) -> Tensor:
+
+    def forward(self, x: Tensor, mask: Tensor = None) -> Tensor:
         # split keys, queries and values in num_heads
         """
         (h d) Representation division between dimension/num_heads
@@ -74,43 +68,47 @@ class MultiHeadAttention(nn.Module):
         h -> representa a quantidade heads geradas
         """
         queries = rearrange(self.queries(x), "b n (h d) -> b h n d", h=self.num_heads)
-        
+
         keys = rearrange(self.keys(x), "b n (h d) -> b h n d", h=self.num_heads)
-        
-        values  = rearrange(self.values(x), "b n (h d) -> b h n d", h=self.num_heads)
+
+        values = rearrange(self.values(x), "b n (h d) -> b h n d", h=self.num_heads)
         # sum up over the last axis
         # matrix multiplication between queries and keys, a.k.a sum up over the last axis.
-        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys) # batch, num_heads, query_len, key_len
-        
+        energy = torch.einsum('bhqd, bhkd -> bhqk', queries, keys)  # batch, num_heads, query_len, key_len
+
         if mask is not None:
             fill_value = torch.finfo(torch.float32).min
             energy.mask_fill(~mask, fill_value)
-            
-        scaling = self.emb_size ** (1/2)
-        
+
+        scaling = self.emb_size ** (1 / 2)
+
         att = F.softmax(energy, dim=-1) / scaling
-        
+
         att = self.att_drop(att)
         # sum up over the third axis
         out = torch.einsum('bhal, bhlv -> bhav ', att, values)
-        
+
         out = rearrange(out, "b h n d -> b n (h d)")
         out = self.projection(out)
         return out
+
 
 class ResidualAdd(nn.Module):
     def __init__(self, fn):
         super().__init__()
         self.fn = fn
-        
+
     def forward(self, x, **kwargs):
         res = x
         x = self.fn(x, **kwargs)
         x += res
         return x
-    
+
+
 class FeedForwardBlock(nn.Sequential):
-    def __init__(self, emb_size: int, expansion: int = 2, drop_p: float = 0.):
+    def __init__(self, emb_size: int = parameters['emb_size'],
+                 expansion: int = parameters['expansion_embbeding'],
+                 drop_p: float = parameters['dropout']):
         super().__init__(
             nn.Linear(emb_size, expansion * emb_size),
             nn.ReLU(),
@@ -118,13 +116,14 @@ class FeedForwardBlock(nn.Sequential):
             nn.Linear(expansion * emb_size, emb_size),
         )
 
+
 class TransformerEncoderBlock(nn.Sequential):
     def __init__(self,
                  emb_size: int = parameters['emb_size'],
                  drop_p: float = parameters['dropout'],
                  forward_expansion: int = parameters['expansion_embbeding'],
                  forward_drop_p: float = parameters['dropout'],
-                 ** kwargs):
+                 **kwargs):
         super().__init__(
             ResidualAdd(nn.Sequential(
                 nn.LayerNorm(emb_size),
@@ -138,30 +137,32 @@ class TransformerEncoderBlock(nn.Sequential):
                 nn.Dropout(drop_p)
             )
             ))
-        
+
+
 class TransformerEncoder(nn.Sequential):
     def __init__(self,
                  depth: int = parameters['depth'],
                  **kwargs):
         super().__init__(*[TransformerEncoderBlock(**kwargs) for _ in range(depth)])
-        
+
+
 class ClassificationHead(nn.Sequential):
     def __init__(self, emb_size: int = parameters['emb_size'], n_classes: int = parameters['n_classes']):
-        
         super().__init__(
             Reduce('b n e -> b e', reduction='mean'),
-            nn.LayerNorm(emb_size), 
+            nn.LayerNorm(emb_size),
             nn.Linear(emb_size, n_classes))
-        
+
+
 class ViT(nn.Sequential):
-    def __init__(self,     
-                in_channels: int = parameters['in_channels'],
-                patch_size: int = parameters['patch_size'],
-                emb_size: int = parameters['emb_size'],
-                img_size: int = parameters['img_size'],
-                depth: int = parameters['depth'],
-                n_classes: int = parameters['depth'],
-                **kwargs):
+    def __init__(self,
+                 in_channels: int = parameters['in_channels'],
+                 patch_size: int = parameters['patch_size'],
+                 emb_size: int = parameters['emb_size'],
+                 img_size: int = parameters['img_size'],
+                 depth: int = parameters['depth'],
+                 n_classes: int = parameters['depth'],
+                 **kwargs):
         super().__init__(
             PatchEmbedding(in_channels, patch_size, emb_size, img_size),
             TransformerEncoder(depth, emb_size=emb_size, **kwargs),
---
+++ transformer_decoder/transformer_decoder.py
@@ -0,0 +1,372 @@
+import torch
+import yaml
+from transformers import AutoTokenizer
+import numpy as np
+
+tokenizer = AutoTokenizer.from_pretrained('allenai/scibert_scivocab_cased')
+
+with open('transformer_decoder/config_decoder.yaml', 'r') as file:
+    parameters = yaml.safe_load(file)
+
+
+class Embedder(torch.nn.Module):
+    def __init__(self, max_seq_length: int = parameters['max_seq_length'],
+                 vocab_size: int = tokenizer.vocab_size,
+                 embbeding=parameters['embbeding'],
+                 pad_token_id: int = tokenizer.pad_token_id):
+        super().__init__()
+
+        self.embed = torch.nn.Embedding(vocab_size,
+                                        embbeding,
+                                        padding_idx=pad_token_id)
+
+        self.embed_pos = torch.nn.Embedding(max_seq_length,
+                                            embbeding,
+                                            padding_idx=pad_token_id)
+
+    def forward(self, x):
+        return self.embed(x) + self.embed_pos.weight
+
+
+class MultiHeadSelfAttention(torch.nn.Module):
+
+    def __init__(self, embedding_dim=parameters['embbeding'],
+                 forward_drop_p=parameters['forward_drop_p'],
+                 max_seq_length: int = parameters['max_seq_length'],
+                 num_heads: int = parameters['num_heads'],
+                 padding_id: int = tokenizer.pad_token_id):
+        super().__init__()
+
+        self.num_heads = num_heads
+        self.padding_id = padding_id
+        self.max_seq_length = max_seq_length
+
+        self.W_q = torch.nn.Linear(embedding_dim, embedding_dim, bias=False)
+        self.W_k = torch.nn.Linear(embedding_dim, embedding_dim, bias=False)
+        self.W_v = torch.nn.Linear(embedding_dim, embedding_dim, bias=False)
+
+        self.W_o = torch.nn.Linear(embedding_dim, embedding_dim, bias=False)
+
+    @staticmethod
+    def self_attention(q, k, v, mask):
+        """Tenho a ponderação de todos os tokens contra todos"""
+        s = torch.matmul(q, k.transpose(-2, -1))
+
+        """preenchimento da mascara"""
+        s = s.masked_fill(mask.unsqueeze(1) == 0, -float("inf"))
+
+        """mascara aplicando softmax"""
+        p = torch.nn.functional.softmax(s, dim=-1)
+
+        att_output = torch.matmul(p, v)
+
+        return att_output.transpose(1, 2)
+
+    def forward(self, inputs, mask):
+        batch_size = inputs.shape[0]
+
+        """
+        Modificar o codigo aqui para entrar o embbeding do modelo encoder.
+        """
+
+        q = self.W_q(inputs)
+        q = q.view(batch_size, self.max_seq_length, self.num_heads, -1)
+        q = q.transpose(1, 2)
+
+        k = self.W_k(inputs)
+        k = k.view(batch_size, self.max_seq_length, self.num_heads, -1)
+        k = k.transpose(1, 2)
+
+        v = self.W_v(inputs)
+        v = v.view(batch_size, self.max_seq_length, self.num_heads, -1)
+        v = v.transpose(1, 2)
+
+        """
+        The embbedings  K V representions the images,
+        should be same dims that Q.
+        """
+
+        att_output = self.self_attention(q, k, v, mask)
+
+        att_output = att_output.reshape(batch_size, self.max_seq_length, -1)
+        att_output = self.W_o(att_output)
+
+        return att_output
+
+
+class TransformerEncoderBlock(torch.nn.Module):
+    def __init__(self,
+                 depth: int = 2,
+                 forward_expansion=parameters['forward_expansion'],
+                 forward_expansion_p=parameters['forward_drop_p'],
+                 emb_size: int = parameters['embbeding'],
+                 drop_p: float = parameters['drop_p'],
+                 **kwargs):
+        super().__init__()
+
+        self.layer_norm = torch.nn.LayerNorm(emb_size)
+        self.multi_head_self_attention = MultiHeadSelfAttention(emb_size, **kwargs)
+        self.dropout = torch.nn.Dropout(drop_p)
+
+    def forward(self, inputs, mask):
+        residue = inputs
+
+        x = self.layer_norm(inputs)
+        x = self.multi_head_self_attention(x, mask)
+        x = self.dropout(x)
+
+        x = x + residue
+
+        return x
+
+
+class TransformerEncoder(torch.nn.Module):
+    def __init__(self,
+                 **kwargs):
+        super().__init__()
+
+        self.layer = TransformerEncoderBlock(**kwargs)
+
+        self.layer_2 = TransformerEncoderBlock(**kwargs)
+
+        self.layer_3 = TransformerEncoderBlock(**kwargs)
+
+    def forward(self, inputs, mask):
+        x = self.layer(inputs, mask)
+
+        x = self.layer_2(x, mask)
+
+        x = self.layer_3(x, mask)
+
+        return x
+
+
+class CreateMask(torch.nn.Module):
+    def __init__(self,
+                 max_seq_length: int = parameters['max_seq_length'],
+                 padding_id: int = tokenizer.pad_token_id):
+        super().__init__()
+        self.max_seq_length = max_seq_length
+        self.padding_id = padding_id
+
+    def forward(self, inputs):
+        batch_size = inputs.shape[0]
+
+        mask = torch.tril(torch.ones(batch_size, self.max_seq_length, self.max_seq_length)).to(device)
+        mask = mask.masked_fill(inputs.unsqueeze(1) == self.padding_id, 0)
+
+        return mask
+
+
+class Classifier(torch.nn.Sequential):
+    def __init__(self,
+                 emb_size: int = parameters['embbeding'],
+                 vocab_size: int = tokenizer.vocab_size):
+        super().__init__(torch.nn.Linear(emb_size, emb_size * 2),
+                         torch.nn.ReLU(),
+                         torch.nn.Linear(emb_size * 2, vocab_size, bias=False))
+
+
+class LanguageModel(torch.nn.Sequential):
+
+    def __init__(self, vocab_size: int,
+                 max_seq_length: int = parameters['max_seq_length'],
+                 dim: int = parameters['embbeding'],
+                 pad_token_id: int = tokenizer.pad_token_id):
+        super().__init__()
+
+        self.pad_token_id = pad_token_id
+
+        self.mask = CreateMask(max_seq_length, pad_token_id)
+
+        self.embedder = Embedder(max_seq_length, vocab_size, dim, pad_token_id)
+
+        self.transformer_encoder = TransformerEncoder(depth=parameters['depth'],
+                                                      emb_size=parameters['embbeding'],
+                                                      drop_p=parameters['drop_p'],
+                                                      forward_expansion=parameters['forward_expansion'],
+                                                      forward_drop_p=parameters['forward_drop_p'],
+                                                      max_seq_length=parameters['max_seq_length'],
+                                                      num_heads=parameters['num_heads'],
+                                                      padding_id=tokenizer.pad_token_id)
+
+        self.classifier = Classifier(dim, vocab_size)
+
+    def forward(self, inputs):
+        mask = self.mask(inputs)
+
+        inputs_embbedings = self.embedder(inputs)
+
+        output_transformers = self.transformer_encoder(inputs_embbedings, mask)
+
+        outputs = self.classifier(output_transformers)
+
+        return outputs
+
+
+if __name__ == "__main__":
+
+    from typing import List
+    from tqdm.notebook import tqdm
+    from torch.utils.data import DataLoader
+
+    if torch.cuda.is_available():
+        dev = "cuda:0"
+    else:
+        dev = "cpu"
+    device = torch.device(dev)
+    print('Using {}'.format(device))
+
+
+    def tokenize(text: str, tokenizer):
+        # Recomenda-se usar o tokenizer.batch_encode_plus pois é mais rápido.
+        return tokenizer(text, return_tensors=None, add_special_tokens=False).input_ids
+
+
+    class MyDataset():
+        def __init__(self, texts: List[str], tokenizer, max_seq_length: int):
+            self.max_seq_length = max_seq_length
+            self.tokenized_texts = []
+            for text in tqdm(texts):
+                tokenized_text = tokenize(f'[CLS] {text}', tokenizer)
+                tokenized_text += [tokenizer.vocab['[PAD]']] * max(0, 1 + max_seq_length - len(tokenized_text))
+
+                for i in range(0, len(tokenized_text) - 1, max_seq_length):
+
+                    if i + max_seq_length < len(tokenized_text):
+                        self.tokenized_texts.append(tokenized_text[i: i + max_seq_length + 1])
+                    else:
+                        self.tokenized_texts.append(tokenized_text[-max_seq_length - 1:])
+
+            self.tokenized_texts = torch.LongTensor(self.tokenized_texts)
+
+        def __len__(self):
+            return len(self.tokenized_texts)
+
+        def __getitem__(self, idx):
+            x_y = self.tokenized_texts[idx]
+            return x_y[:-1], x_y[1:]
+
+
+    seq_length = 9
+
+    max_seq_length = seq_length
+
+    texts = open('transformer_decoder/sample-1gb.txt').readlines()
+
+    len_max = int(len(texts) / 200)
+
+    train_examples = int(len_max * 0.6)
+    valid_examples = int(len_max * 0.3)
+    test_examples = int(len_max * 0.1)
+
+    print(f"train examples: {train_examples}")
+    print(f"valid examples: {valid_examples}")
+    print(f"test examples: {test_examples}")
+
+    print(f'Read {len(texts)} lines.')
+
+    max_lines = train_examples + valid_examples + test_examples
+    print(f'Truncating to {max_lines} lines.')
+    texts = texts[:max_lines]
+
+    training_texts = texts[:-(valid_examples + test_examples)]
+    valid_texts = texts[-(valid_examples + test_examples):-test_examples]
+    test_texts = texts[-test_examples:]
+
+    training_dataset = MyDataset(texts=training_texts, tokenizer=tokenizer, max_seq_length=max_seq_length)
+    valid_dataset = MyDataset(texts=valid_texts, tokenizer=tokenizer, max_seq_length=max_seq_length)
+    test_dataset = MyDataset(texts=test_texts, tokenizer=tokenizer, max_seq_length=max_seq_length)
+
+    model = LanguageModel(
+        vocab_size=tokenizer.vocab_size,
+        max_seq_length=max_seq_length,
+        dim=64,
+        pad_token_id=tokenizer.pad_token_id,
+    ).to(device)
+
+    sample_input, _ = next(iter(DataLoader(training_dataset, batch_size=1)))
+    sample_input = sample_input.to(device)
+    sample_output = model(sample_input)
+    print(f'sample_input.shape: {sample_input.shape}')
+    print(f'sample_output.shape: {sample_output.shape}')
+
+    max_examples = 150_000_000
+    eval_every_steps = 1000
+    lr = 3e-4
+
+    train_loader = DataLoader(training_dataset, batch_size=255, shuffle=True, drop_last=True)
+    validation_loader = DataLoader(valid_dataset, batch_size=255)
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer,
+                                                           mode='min',
+                                                           factor=0.2,
+                                                           patience=0,
+                                                           verbose=True)
+
+
+    def train_step(input_ids, target_ids):
+
+        model.train()
+        model.zero_grad()
+
+        logits = model(input_ids)
+        logits = logits.reshape(-1, logits.shape[-1])
+
+        target_ids = target_ids.reshape(-1)
+
+        loss = torch.nn.functional.cross_entropy(logits, target_ids, ignore_index=model.pad_token_id)
+        loss.backward()
+
+        optimizer.step()
+
+        return loss.item()
+
+
+    def validation_step(input_ids, target_ids):
+
+        model.eval()
+
+        logits = model(input_ids)
+        logits = logits.reshape(-1, logits.shape[-1])
+
+        target_ids = target_ids.reshape(-1)
+
+        loss = torch.nn.functional.cross_entropy(logits, target_ids, ignore_index=model.pad_token_id)
+
+        return loss.item()
+
+
+    train_losses = []
+    n_examples = 0
+    step = 0
+
+    print("Training inicialize")
+
+    while n_examples < max_examples:
+
+        for train_input_ids, train_target_ids in tqdm(train_loader):
+
+            loss = train_step(train_input_ids.to(device), train_target_ids.to(device))
+            train_losses.append(loss)
+
+            if step % eval_every_steps == 0:
+                train_ppl = np.exp(np.average(train_losses))
+
+                with torch.no_grad():
+                    valid_ppl = np.exp(np.average([
+                        validation_step(val_input_ids.to(device), val_target_ids.to(device))
+                        for val_input_ids, val_target_ids in validation_loader]))
+
+                print(
+                    f'{step} steps; {n_examples} examples so far; train ppl: {train_ppl:.2f}, valid ppl: {valid_ppl:.2f}')
+                train_losses = []
+
+                scheduler.step(valid_ppl)
+
+            n_examples += len(train_input_ids)  # Increment of batch size
+            step += 1
+            if n_examples >= max_examples:
+                break
